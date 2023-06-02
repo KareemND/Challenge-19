c8dd2337d0812c9553f201d8581c64e938659395
KareemND <119475435+KareemND@users.noreply.github.com>
2023-06-02 19:53:10 -0400

    edited the files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,18 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readwrite');
-  tx.store.add({content});
-  await tx.done;
-};
+  export const putDb = async (content) => {
+    const contactDb = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  const db = await initdb();
-  return db.getAll('jate');
-};
+    const tx = contactDb.transaction('jate', 'readwrite');
 
-initdb();
+    const store = tx.objectStore('jate');
+    
+    const request = store.put({ id: 1, value: content});
+    
+    const result = await request;
+  };
+
+  export const getDb = async () => {
+
+    const contactDb = await openDB('jate', 1);
+
+    const tx = contactDb.transaction('jate', 'readonly');
+
+    const store = tx.objectStore('jate');
+
+    const request = store.getAll();
+
+   const result = await request;
+
+   return result?.value;
+  };
+  
+  initdb();
+  
+ 
+
+  
